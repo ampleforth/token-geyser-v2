1fa681d44451544d6dbe3e17e2b9da8ccd54af09
Stephane <thegostep@gmail.com>
2020-11-11 08:36:55 +0100

    make stake ownership transferable (#23)
    * make stake ownership transferable
    Fixes #2
    
    * Fix vault deployment flag

--- contracts/Vault/Vault.sol
+++ contracts/Vault/Vault.sol
@@ -5,10 +5,10 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/Initializable.sol";
 
-interface IUserVault {
+interface IVault {
     function initialize(
         address stakingToken,
-        address user,
+        address owner,
         uint256 geyserID
     ) external;
 
@@ -17,25 +17,28 @@ interface IUserVault {
         address to,
         uint256 value
     ) external;
+
+    function getOwner() external view returns (address owner);
 }
 
-/// @title User Vault
+/// @title Vault
 /// @dev Security contact: dev-support@ampleforth.org
-// todo: #15 consider adding support for other token standards to user vault
-// todo: #16 consider adding support for ERC1271 to user vault
-contract UserVault is IUserVault, Ownable, Initializable {
-    // todo: #17 consider packing user vault storage
+// todo: #15 consider adding support for other token standards to vault
+// todo: #16 consider adding support for ERC1271 to vault
+contract Vault is IVault, Ownable, Initializable {
+    // todo: #17 consider packing vault storage
     address public _stakingToken;
     address public _geyser;
     uint256 public _geyserID;
 
     function initialize(
         address stakingToken,
-        address user,
+        address owner,
         uint256 geyserID
     ) public override initializer {
         // set initialization data
-        Ownable.transferOwnership(user);
+        // todo: #21 set ownership directly to user instead of factory to save 5k gas per deploy
+        Ownable.transferOwnership(owner);
         _stakingToken = stakingToken;
         _geyser = msg.sender;
         _geyserID = geyserID;
@@ -49,19 +52,20 @@ contract UserVault is IUserVault, Ownable, Initializable {
         // validate access control
         if (token == _stakingToken) {
             // only the geyser can transfer the staking tokens
-            require(msg.sender == _geyser, "UserVault: only geyser can transfer staking token");
+            require(msg.sender == _geyser, "Vault: only geyser can transfer staking token");
         } else {
             // only the owner can transfer all other tokens
-            require(
-                msg.sender == Ownable.owner(),
-                "UserVault: only owner can transfer other tokens"
-            );
+            require(msg.sender == Ownable.owner(), "Vault: only owner can transfer other tokens");
         }
 
         // cannot transfer tokens back to the vault
-        require(to != address(this), "UserVault: cannot transfer tokens back to the vault");
+        require(to != address(this), "Vault: cannot transfer tokens back to the vault");
 
         // transfer tokens
-        require(IERC20(token).transfer(to, value), "UserVault: token transfer failed");
+        require(IERC20(token).transfer(to, value), "Vault: token transfer failed");
+    }
+
+    function getOwner() external view override returns (address owner) {
+        return Ownable.owner();
     }
 }

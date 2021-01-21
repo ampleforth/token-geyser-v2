3e8c2b2ec1afc1c61bee2aa2724a4656e2024694
thegostep <thegostep@gmail.com>
2021-01-21 16:54:01 +0000

    add owner to mockERC1271

--- contracts/Mock/MockERC1271.sol
+++ contracts/Mock/MockERC1271.sol
@@ -6,8 +6,8 @@ import {ERC1271} from "../Access/ERC1271.sol";
 contract MockERC1271 is ERC1271 {
     address public owner;
 
-    constructor() {
-        owner = msg.sender;
+    constructor(address _owner) {
+        owner = _owner;
     }
 
     function _getOwner() internal view override returns (address) {

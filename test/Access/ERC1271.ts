25cb492cd747d5a41989b88c6e3608434dcba2b9
thegostep <thegostep@gmail.com>
2021-01-27 16:30:26 +0000

    match 1271 implementation to OpenZeppelin Fixes #120

--- test/Access/ERC1271.ts
+++ test/Access/ERC1271.ts
@@ -1,16 +1,24 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 import { expect } from 'chai'
 import { Contract } from 'ethers'
+import { arrayify, hashMessage, keccak256 } from 'ethers/lib/utils'
 import { ethers } from 'hardhat'
 import { deployContract } from '../utils'
 
 describe('ERC1271', function () {
   let accounts: SignerWithAddress[]
   let MockERC1271: Contract
-  const message = 'ERC1271 test message'
-  const messageHash = ethers.utils.arrayify(ethers.utils.hashMessage(message))
+  const message = hashMessage('ERC1271 test message')
   let VALID_SIG: string
-  const INVALID_SIG = '0xffffffff'
+  const INVALID_SIG = '0x00000000'
+
+  function toEthSignedMessageHash(messageHex: string) {
+    const messageBuffer = Buffer.from(messageHex.substring(2), 'hex')
+    const prefix = Buffer.from(
+      `\u0019Ethereum Signed Message:\n${messageBuffer.length}`,
+    )
+    return keccak256(Buffer.concat([prefix, messageBuffer]))
+  }
 
   beforeEach(async function () {
     // prepare signers
@@ -24,24 +32,30 @@ describe('ERC1271', function () {
 
   describe('isValidSignature', function () {
     it('should return error value if signed by account other than owner', async function () {
-      const sig = await accounts[1].signMessage(messageHash)
-      expect(await MockERC1271.isValidSignature(messageHash, sig)).to.eq(
-        INVALID_SIG,
-      )
+      const sig = await accounts[1].signMessage(arrayify(message))
+      expect(
+        await MockERC1271.isValidSignature(
+          toEthSignedMessageHash(message),
+          sig,
+        ),
+      ).to.eq(INVALID_SIG)
     })
 
     it('should revert if signature has incorrect length', async function () {
-      const sig = await accounts[0].signMessage(messageHash)
+      const sig = await accounts[0].signMessage(arrayify(message))
       expect(
-        MockERC1271.isValidSignature(messageHash, sig.slice(0, 10)),
+        MockERC1271.isValidSignature(
+          toEthSignedMessageHash(message),
+          sig.slice(0, 10),
+        ),
       ).to.be.revertedWith('ECDSA: invalid signature length')
     })
 
     it('should return success value if signed by owner', async function () {
-      const sig = await accounts[0].signMessage(messageHash)
-      expect(await MockERC1271.isValidSignature(messageHash, sig)).to.eq(
-        VALID_SIG,
-      )
+      const sig = await accounts[0].signMessage(arrayify(message))
+      expect(
+        await MockERC1271.isValidSignature(hashMessage(message), sig),
+      ).to.eq(VALID_SIG)
     })
   })
 })

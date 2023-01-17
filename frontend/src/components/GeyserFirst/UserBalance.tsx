4ab2fbbf77ec2e8998ca64466296b7a06695558c
nithin <nithin@fragments.org>
2023-01-17 16:53:21 +0530

    updated urls

--- frontend/src/components/GeyserFirst/UserBalance.tsx
+++ frontend/src/components/GeyserFirst/UserBalance.tsx
@@ -9,16 +9,21 @@ interface Props {
   decimals: number
   symbol: string
   isStakingAction: boolean
+  poolAddress?:string
 }
 
-export const UserBalance: React.FC<Props> = ({ parsedAmount, currentAmount, decimals, symbol, isStakingAction }) => {
-  const formatDisplayAmount = (amt: BigNumberish) => formatAmount(amt, decimals)
+export const UserBalance: React.FC<Props> = ({ parsedAmount, currentAmount, decimals, symbol, isStakingAction, poolAddress }) => {
+  const formatDisplayAmount = (amt: BigNumberish, sym:string) => (
+    <a href={poolAddress} target="_blank" rel="noreferrer">
+      {formatAmount(amt, decimals)} {sym}
+    </a>
+  )
   return (
     <FlexDiv>
       {parsedAmount.isZero() ? (
-        <Text>{isStakingAction ? 'Available' : 'Staked'} balance: {formatDisplayAmount(currentAmount)} ({symbol})</Text>
+          <Text>{isStakingAction ? 'Available' : 'Staked'} balance: {formatDisplayAmount(currentAmount, symbol)}</Text>
       ) : (
-        <Text>New {isStakingAction ? 'available' : 'stake'} balance: {formatDisplayAmount(currentAmount.sub(parsedAmount))} ({symbol})</Text>
+        <Text>New {isStakingAction ? 'available' : 'stake'} balance: {formatDisplayAmount(currentAmount.sub(parsedAmount), symbol)}</Text>
       )}
     </FlexDiv>
   )

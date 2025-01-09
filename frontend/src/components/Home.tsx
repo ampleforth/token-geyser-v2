714179597eea2c382c3ace8e88c0b90e799704e7
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2025-01-09 15:54:54 -0500

    showing some expired geysers with high tvl

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -56,7 +56,9 @@ export const Home = () => {
 
   const totalTVL = geyserData.reduce((s, g) => g.tvl + s, 0)
   const totalRewards = geyserData.filter((g) => g.active).reduce((s, g) => g.rewards + s, 0)
-  let geysersToShow = geyserData.filter((g) => g.active || stakedGeysers.find((s) => s.id === g.id))
+  let geysersToShow = geyserData.filter(
+    (g) => (g.active || (g.tvl > 250000 && g.apy > 0)) || stakedGeysers.find((s) => s.id === g.id),
+  )
   if (geysersToShow.length === 0) {
     geysersToShow = geyserData.slice(0, 3)
   }
